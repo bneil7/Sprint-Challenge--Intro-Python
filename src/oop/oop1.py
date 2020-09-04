8970bf0b6d12b47d74017cab433bb960c89b982f
Brandon Neil <63384257+bneil7@users.noreply.github.com>
2020-09-04 09:37:28 -0600

    finished oop1 and 2

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,37 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+### BASE CLASS ###
+class Vehicle:
+    pass
+
+
+### child 1: ground ###
+class GroundVehicle(Vehicle):
+    pass
+
+
+# ground child 1
+class Car(GroundVehicle):
+    pass
+
+
+# ground child 2
+class Motorcycle(GroundVehicle):
+    pass
+
+
+### child 2: flight ###
+class FlightVehicle(Vehicle):
+    pass
+
+
+# flight child 1
+class Starship(FlightVehicle):
+    pass
+
+
+# flight child 2
+class Airplane(FlightVehicle):
+    pass
